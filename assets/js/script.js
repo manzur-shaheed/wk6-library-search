430798045d9fb0ea3ba3d92338306942699ca718
ms <shaheed_manzur@yahoo.com>
2021-05-03 14:57:26 -0400

    added results page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
 var searchEl = $('#search');
 
+// main page section
 // add event handler
-searchEl.on('submit', searchLibrary);
+searchEl.on('submit', searchLibraryMain);
 
-function searchLibrary(event) {
+function searchLibraryMain(event) {
+    var serachText, formatText, queryStr;
+
     event.preventDefault();
+    serachText = $('#searchText').val();
+    formatText = $('#format').val();
 
-    console.log('I am here');
+    // console.log(serachText);
+    // console.log(formatText);
+    if (serachText === '') {
+        alert('Please enter a search text!');
+        return;
+    }
+    queryStr = "./results.html?searchText=" + serachText + "&format=" + formatText;
+    location.assign(queryStr);
 }
+
+// result page section
